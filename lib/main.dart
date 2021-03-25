f0dac12fac20ceab0cbab4ab56a0eef9e0fdeaba
RohanUttamVeer <rohanveer04@gmail.com>
2021-03-25 11:26:57 +0530

    Elevated button not working

--- lib/main.dart
+++ lib/main.dart
@@ -9,27 +9,12 @@ class basicFlutter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        child: RaisedButton(
-          onPressed: () {},
-          child: Text('hello'),
-        ),
+      home: Row(
+        children: [ElevatedButton(onPressed: () {},
+        style: ElevatedButton.styleFrom(elevation: 10),
+        child: Text(' Elevated Button'))
+        ],
       ),
     );
   }
 }
-ignore: camel_case_types
-class basicFlutter_ extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(10),
-            child:ElevatedButton(onPressed: () {},
-                style: ElevatedButton.styleFrom(elevation: 10),
-                child: Text(' Elevated Button'))
-        ),
-      ),
-    );
-  }
